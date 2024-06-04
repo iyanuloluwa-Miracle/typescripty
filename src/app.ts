179f970cfa0a706cc18dd99b691cf9ac3f715b4c
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-06-04 14:04:12 +0100

    :bug: cookie-parser fixes

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { connectToDb } from "./utils";
 import http from "http";
 import { initSocket } from "./sockets/socket.service";
 import { userRouter } from './routes';
-import { useErrorHandler, useNotFound, useRateLimiter } from "./middlewares/";
+import { useErrorHandler, useNotFound, useRateLimiter } from "./middlewares";
 
 
 
@@ -19,15 +19,28 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+const server = http.createServer(app);
+initSocket(server);
 
 // Middleware
+app.use(cors());
 app.use(morgan('dev'));
+app.use(useNotFound);
+app.use(useErrorHandler);
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(useRateLimiter);
 
 // Routes
 app.get('/', (req, res) => {
     res.send({ message: "Welcome to typescripty!😁" });
 });
+
+//endpoints
+app.use("/api/user", userRouter);
+
+
 
 // Start the server
 app.listen(port, () => {
---
+++ src/middlewares/auth.ts
@@ -0,0 +1,66 @@
+import { NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { response } from "../utils";
+
+const useAuth = (req: any, res: any, next: NextFunction) => {
+  const tokenFromCookie = req.cookies.accessToken;
+  const refreshTokenCookie = req.cookies.refreshToken;
+
+
+
+  //just in case of logout  
+  if (!refreshTokenCookie) {
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+    return response(res, 401, "Access denied, no refresh token");
+  }
+
+  if (!tokenFromCookie) {
+    return response(
+      res,
+      401,
+      "You're not authorized to perform this action, no access token!"
+    );
+  }
+
+  try {
+    // Extract the bearer token from the header
+    const JWT_SECRET: any = process.env.JWT_PRIVATE_KEY;
+
+    if (!JWT_SECRET) {
+      throw new Error("JWT private key is missing.");
+    }
+
+    let decodeCookie: any = jwt.verify(tokenFromCookie, JWT_SECRET);
+    if (decodeCookie && decodeCookie._id) {
+      const userRole = "user",
+        hospitalRole = "hospital";
+      if (decodeCookie.role === userRole) {
+        req.user = decodeCookie;
+        res.user = decodeCookie;
+        req.userType = "user";
+        next();
+      } else if (decodeCookie.role === hospitalRole) {
+        req.hospital = decodeCookie;
+        res.hospital = decodeCookie;
+        req.userType = "hospital";
+
+        next();
+      } else {
+        console.log("invalid auth token!");
+        return response(res, 401, "Invalid auth token");
+      }
+    } else {
+      return response(res, 401, "Invalid auth token.");
+    }
+  } catch (error) {
+    console.error(error);
+    return response(
+      res,
+      401,
+      `You're not authorized to perform this action! ${error}`
+    );
+  }
+};
+
+export default useAuth;
